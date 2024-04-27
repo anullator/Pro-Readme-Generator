3ebef15fc153b048de5607e16cb35bd90150ffd7
anullator <danyelle.joy@gmail.com>
2024-04-27 17:17:03 -0500

    created markdown template outline

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -22,7 +22,89 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
+    \n\n## Description
 
+    \n\n${data.description}
+    
+    \n\n## Table of Contents (Optional)
+    
+    \n\n${data['table-of-contents']}
+    
+      //TODO: Render dynamically based on sections added
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [Credits](#credits)
+    - [License](#license)
+      [Badges](#badges)
+      [Features](#features)
+      [How To Contribute](#how-to-contribute)
+      [Tests](#tests)
+    
+    \n\n## Installation
+    
+    \n\n${data.installation}
+    
+    \n\n## Usage
+    
+    \n\n${data.usage}
+//    Provide instructions and examples for use. Include screenshots as needed.
+    
+//    To add a screenshot, create an assets/images folder in your repository and upload your screenshot to it. Then, using the relative file path, add it to your README using the following syntax:
+    
+//    ![alt text](assets/images/screenshot.png)
+    
+//    This is the link to the deployed webpage: _webpage url_
+    
+    \n\n## Credits
+
+    \n\n${data.credits}
+    
+//    List your collaborators, if any, with links to their GitHub profiles.
+    
+//    If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
+    
+//    If you followed tutorials, include links to those here as well.
+    
+    \n\n## License
+    
+    \n\n${data.license}
+
+    //TODO: create array of license markdown for each license
+    
+//    MIT License
+    
+ //   Copyright (c) 2024 Dany Goodwin
+    
+//    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+    
+//    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+    
+//    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+    
+    \n\n## Badges
+
+    \n\n${data.badges}
+
+    TODO: create a checkbox for badges so I can dynamically add and render any applicable badges
+    
+//    ![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
+    
+//    Badges aren't necessary, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+    
+    \n\n ## Features
+    
+    \n\n${data.features}
+    
+    \n\n## How to Contribute
+    
+    \n\n${data.contribute}
+//    If you created an application or package and would like other developers to contribute to it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+    
+    \n\n## Tests
+
+    \n\n${data.tests}
+    
+//    Go the extra mile and write tests for your application. Then provide examples on how to run them here.
 `;
 }
 
