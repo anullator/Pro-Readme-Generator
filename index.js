67395b34bdc5a660da2285aa9896049c85733489
anullator <danyelle.joy@gmail.com>
2024-05-01 22:11:33 -0500

    prompts essential questions and adds question section to readme

--- index.js
+++ index.js
@@ -41,33 +41,26 @@ const questions = [
             return input ? true : console.error('\n\u{26A0} Error: Must provide usage instructions');
         }
     },
-        // {
-    //     type: 'input select checkbox confirm password expand editor rawlist '
-    //     name: 'for variable'
-    //     message:
-    //     default:
-    //     choices: 'for select type'
-    //     validate: 
-    //     when: 'add function to only ask the question under certain conditions'
-    // },
-        // {
-    //     type: 'input select checkbox confirm password expand editor rawlist '
-    //     name: 'for variable'
-    //     message:
-    //     default:
-    //     choices: 'for select type'
-    //     validate: 
-    //     when: 'add function to only ask the question under certain conditions'
-    // },
-        // {
-    //     type: 'input select checkbox confirm password expand editor rawlist '
-    //     name: 'for variable'
-    //     message:
-    //     default:
-    //     choices: 'for select type'
-    //     validate: 
-    //     when: 'add function to only ask the question under certain conditions'
-    // },
+// CREDITS
+    {
+        type: 'list',
+        name: 'addCredits',
+        message: 'Do you want to add credits for others?',
+        default: 'Yes',
+        choices: ['Yes', 'No']
+    },
+    {
+        type: 'input',
+        name: 'credits',
+        message: 'Enter credits for others',
+        validate: function (input) {
+            return input ? true : console.error('\n\u{26A0} Error: Must provide credits');
+        },
+        when: (answers) => {
+            return answers.addCredits === 'Yes';
+        }
+    },
+// LICENSE
     {
         type: 'list',
         message: 'What is the license for this project?',
@@ -75,19 +68,104 @@ const questions = [
         choices: ['Apache 2.0', 'MIT', 'GNU GPL v3', 'CC0 1.0', 'Eclipse Public License 1.0'],
         default: 'MIT'
     },
-    // 'What are the contribution guidelines?',
-    // 'What are the testing instructions?',
-    // 'Enter all questions:',
-    // 'What is your github username?',
-    // 'What is your email address?'
-
-//  TABLE OF CONTENTS PROMPT - keep ToC questions last!!!!
+// FEATURES
+    {
+        type: 'list',
+        name: 'addFeatures',
+        message: 'Do you want to add features list?',
+        default: 'Yes',
+        choices: ['Yes', 'No']
+    },
+    {
+        type: 'input',
+        name: 'features',
+        message: 'Enter features to list:',
+        validate: function (input) {
+            return input ? true : console.error('\n\u{26A0} Error: Must provide features');
+        },
+        when: (answers) => {
+            return answers.addFeatures === 'Yes';
+        }
+    },
+// HOW TO CONTRIBUTE
+    {
+        type: 'input',
+        name: 'contribute',
+        message: 'What are the contribution guidelines?',
+        validate: function (input) {
+            return input ? true : console.error('\n\u{26A0} Error: Must provide features');
+        }
+    },
+// TESTS
+    {
+        type: 'list',
+        name: 'addTests',
+        message: 'Do you want to add testing instructions?',
+        default: 'Yes',
+        choices: ['Yes', 'No']
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Enter testing instructions:',
+        validate: function (input) {
+            return input ? true : console.error('\n\u{26A0} Error: Must provide testing instructions');
+        },
+        when: (answers) => {
+            return answers.addTests === 'Yes';
+        }
+    },
+// BADGES
+    {
+        type: 'list',
+        name: 'addBadges',
+        message: 'Do you want to add any badges?',
+        default: 'Yes',
+        choices: ['Yes', 'No']
+    },
+    {
+        type: 'checkbox',
+        name: 'badges',
+        message: 'Select badges to add:',
+        choices: ['[placeholder 1]', '[placeholder 2]'],
+        when: (answers) => {
+            return answers.addTests === 'Yes';
+        }
+    },
+//  TABLE OF CONTENTS
     {
         type: 'list',
         name: 'table-of-contents',
         message: 'Create a Table of Contents?',
-        choices: ['Yes', 'No'],
         default: 'Yes',
+        choices: ['Yes', 'No']
+    },
+// GITHUB USERNAME
+    {
+        type: 'input',
+        name: 'githubUsername',
+        message: 'Enter your GitHub username:',
+        validate: function (input) {
+            return input ? true : console.error('\n\u{26A0} Error: Must enter GitHub username');
+        }
+    },
+// EMAIL
+    {
+        type: 'input',
+        name: 'email',
+        message: 'Enter your email address:',
+        validate: function (input) {
+            return input ? true : console.error('\n\u{26A0} Error: Must provide email address');
+        }
+    },
+// CONTACT
+    {
+        type: 'input',
+        name: 'contact',
+        message: 'How can a user contact you with questions?',
+        validate: function (input) {
+            return input ? true : console.error('\n\u{26A0} Error: Must provide contact instructions');
+        }
     },
 ];
 
